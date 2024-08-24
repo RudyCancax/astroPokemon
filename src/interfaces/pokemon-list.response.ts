67e2d2971ec1231d6fd3d9b66d70c09ffd48214d
rudycancax <44583578+RudyCancax@users.noreply.github.com>
2024-08-24 16:21:21 -0600

    Feat: Added custom pages, load a page by pokemon in path /pokemons/id (while id is a number from 1 to 151), loads its information dinamically.

---
+++ src/interfaces/pokemon-list.response.ts
@@ -0,0 +1,10 @@
+export interface PokemonListResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Pokemon[];
+}
+export interface Pokemon {
+  name: string;
+  url: string;
+}
